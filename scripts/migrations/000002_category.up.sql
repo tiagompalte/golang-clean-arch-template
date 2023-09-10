400b3e057ea78297e263593e84e671c4513e9a57
Tiago M Palte <tiagompalte@hotmail.com>
2023-09-09 21:01:18 -0300

    create user entity

--- scripts/migrations/000002_category.up.sql
+++ scripts/migrations/000002_category.up.sql
@@ -2,12 +2,18 @@ CREATE TABLE IF NOT EXISTS tb_category (
     id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY
     , created_at    DATETIME NOT NULL DEFAULT NOW()
     , updated_at    DATETIME NOT NULL DEFAULT NOW() ON UPDATE NOW()
-    , deleted_at    DATETIME NOT NULL DEFAULT '0000-00-00'
+    , deleted_at    DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00'
     , slug          VARCHAR(50) NOT NULL
     , name          VARCHAR(50) NOT NULL
+    , user_id       INT UNSIGNED NOT NULL
 
     , UNIQUE INDEX `slug_uidx` ( 
         `slug`          ASC
         , `deleted_at`  ASC
     )
+    , CONSTRAINT fk_category_user_id
+        FOREIGN KEY (user_id)
+        REFERENCES tb_user (id)
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION
 );
